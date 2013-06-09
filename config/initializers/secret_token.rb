68a17f5bfcdf907695cd13ec77a6c522f6f29a2d
Emma Lindsay <emma@flickme.com>
2013-06-08 19:41:35 -0700

    Basic rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Relaxotron::Application.config.secret_token = 'f0879f50bc911446e92da7d7d9fc5bcc0941267249e01aa2234d31d34fb91bd3ad6f6202fd966a234f75e6f0211266ea0aed424c155fb5e26d464e38fff35aba'
