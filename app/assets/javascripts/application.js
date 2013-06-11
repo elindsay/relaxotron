7f1ee0e6f4a58d8d7b9d6ec99615d52b79528df1
Emma Lindsay <emma@flickme.com>
2013-06-10 19:43:57 -0700

    Updates prototype with new videos and fading out

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,9 +14,49 @@
 //= require jquery_ujs
 //= require_tree .
 
+var futureFade = 0;
+var fadingIn = false;
+
+function beforeMovement(carousel, item, idx, state)
+{
+}
+function afterMovement(carousel, item, idx, state)
+{
+  $(".jcarousel-item").each( function(index, value) {
+    $(value).find("video").get(0).pause();
+  });
+
+  $(".jcarousel-item-"+idx+" video").get(0).play();
+}
+
+function fadeOutFadeables(){
+  $(".fadeable").animate({opacity: 0},500);
+}
+
+function fadeInFadeables(){
+  fadingIn = true;
+  $(".fadeable").animate({opacity: 1},500, function() { fadingIn = false; });
+}
+
 $(document).ready(function() {
-    $('.carousel').jcarousel({
-      scroll: 1
-    });
+  futureFade = setTimeout(function() { fadeOutFadeables(); }, 3000);
+  $('.carousel').jcarousel({
+    scroll: 1,
+    wrap: "both",
+    itemVisibleInCallback: {
+      onBeforeAnimation:  beforeMovement,
+      onAfterAnimation:  afterMovement
+    }
+    
+  });
+
+  $("body").mousemove(function(){
+    clearTimeout(futureFade);
+    if($(".fadeable").css('opacity') < 1 && !fadingIn){
+      fadeInFadeables();
+    }
+    futureFade = setTimeout(function() { fadeOutFadeables(); }, 3000);
+  });
 });
+
 
