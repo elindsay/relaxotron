d130aaf40d477f81fee79411e80dff7bfe2dce5d
Emma Lindsay <emma@flickme.com>
2013-07-16 23:14:30 -0700

    Try fixing url thing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Relaxotron::Application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
+    :url => ":s3_domain_url",
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
