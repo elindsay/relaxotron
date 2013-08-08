57f1d9f771c6ed81595e37707703a7ab24086432
Emma Lindsay <emma@flickme.com>
2013-08-07 22:59:32 -0700

    Basic redo of editor page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def title_helper
+    ["Placidity",
+     [@video.try(:name) ? "Video: #{@video.name}" : nil,
+      @sound_clip.try(:name) ? "Audio: #{@sound_clip.name}" : nil].
+     compact.join(", ")].
+    reject{|e| e.empty?}.join(" - ")
+  end
 end
