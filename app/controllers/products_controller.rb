793b52d376894349db27fac71346dfaade788e51
Emma Lindsay <emma@flickme.com>
2013-10-09 18:17:19 -0700

    Basic styling for marketplace

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.where("stock_count >= ?", 1)
+    @product_matrix = Product.where("stock_count >= ?", 1).each_slice(3).to_a.map{|sa| sa[2] = sa[2]; sa}.transpose.map{|sa| sa.compact}
   end
 end
