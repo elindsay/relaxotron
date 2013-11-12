eb1af64d762f60b0ee9859f8423e85affa3102ea
Emma Lindsay <emma@flickme.com>
2013-11-12 12:38:51 -0800

    Make front page legal

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,7 @@ class HomeController < ApplicationController
   def index
     @zone = Zone.all.sample
     @article_one = Article.find(1)
+    @article_two = Article.find(2)
+    @article_three = Article.find(3)
   end
 end
